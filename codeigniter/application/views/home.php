5a4793250c54aa1b8818f9a0264a749b26be1368
Ben Walker <ben.walker18@gmail.com>
2017-12-18 15:21:19 -0800

    initial home page outline

--- codeigniter/application/views/home.php
+++ codeigniter/application/views/home.php
@@ -1,1 +1,35 @@
-Hello, I made this, Save as home
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>TinderLiker</title>
+<style type="text/css">
+
+::selection { background-color: #E13300; color: white; }
+::-moz-selection { background-color: #E13300; color: white; }
+
+body {
+	background-color: #fff;
+	margin: 40px;
+	font: 13px/20px normal Helvetica, Arial, sans-serif;
+	color: #4F5155;
+}
+
+#button {
+	margin: 10px;
+}
+
+</style>
+</head>
+<body>
+<div id="container">
+  <div id="button">
+  <button type="button">Start TinderLiker</button>
+  </div>
+  <div id="logger">
+  </div>
+ </div>
+</body>
+</html>
