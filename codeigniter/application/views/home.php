bd2a5283692adc316d780c92f5a7b7eaa1c36e17
Ben Walker <ben.walker18@gmail.com>
2017-12-18 17:50:29 -0800

    formatting & css changes

--- codeigniter/application/views/home.php
+++ codeigniter/application/views/home.php
@@ -3,33 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<title>TinderLiker</title>
-<style type="text/css">
+	<meta charset="utf-8">
+	<title>TinderLiker</title>
+	<style type="text/css">
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
+	body {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: #fff;
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
-
-#button {
-	margin: 10px;
-}
-
-</style>
+	</style>
 </head>
 <body>
-<div id="container">
-  <div id="button">
-  <button type="button">Start TinderLiker</button>
-  </div>
-  <div id="logger">
-  </div>
- </div>
+	<div id="container">
+		<div id="button">
+			<button type="button">Start TinderLiker</button>
+		</div>
+		<div id="logger">
+		</div>
+	</div>
 </body>
 </html>
